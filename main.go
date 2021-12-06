9ccf52ef9c3676c776ae139666a5fd5989c4ec8e
leejoys <44557202+leejoys@users.noreply.github.com>
2021-12-06 19:16:00 +0200

    read csv

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
+func main() {
+	in, err := os.ReadFile("./data.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(strings.NewReader(string(in)))
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(records)
 }
